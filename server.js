55bc1190c79004eec4246efa650896b05f1239c2
pupinupi <cool.lg-lg@yandex.ru>
2026-08-03 17:06:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-app.use(express.static("."));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.listen(3000, () => {
+// отдаём всю папку client
+app.use(express.static(path.join(__dirname, "client")));
+
+// главная страница
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server started");
 });
