f1da2350cb39d27ab7332b894f3a61389bbc1b31
pupinupi <cool.lg-lg@yandex.ru>
2026-08-03 22:34:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,66 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// отдаём всю папку client
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
 app.use(express.static(path.join(__dirname, "client")));
 
-// главная страница
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
+const rooms = {};
+
+io.on("connection", socket => {
+
+  console.log("Игрок подключился");
+
+  socket.on("createRoom", ({ roomCode, player }) => {
+
+    rooms[roomCode] = {
+      players: [player]
+    };
+
+    socket.join(roomCode);
+
+    io.to(roomCode).emit(
+      "playersUpdate",
+      rooms[roomCode].players
+    );
+  });
+
+  socket.on("joinRoom", ({ roomCode, player }) => {
+
+    if (!rooms[roomCode]) return;
+
+    rooms[roomCode].players.push(player);
+
+    socket.join(roomCode);
+
+    io.to(roomCode).emit(
+      "playersUpdate",
+      rooms[roomCode].players
+    );
+  });
+
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server started");
 });
